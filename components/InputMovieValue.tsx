1452c4ee80e8faee47222b45816f613ccc7eb31b
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-11-14 16:39:01 +0900

    add movie input

--- components/InputMovieValue.tsx
+++ components/InputMovieValue.tsx
@@ -1,3 +1,24 @@
+import { IconButton, InputBase } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import CancelIcon from '@mui/icons-material/Cancel';
+import CardContent from '@mui/material/CardContent';
+
 export const InputMovieValue = (props) => {
-  const { searchValue } = props;
+  const { searchValue, resetInput, handleInputChange, searchMovie } = props;
+
+  return (
+    <div id="inputBase">
+    <InputBase
+      placeholder="映画タイトル入力"
+      sx={{ ml: 2, flex: 1 }}
+      onChange={handleInputChange}
+      value={searchValue}
+    />
+    { searchValue ?
+      <CancelIcon sx={{ ml: 1, mt: -1, my: -1 }} onClick={resetInput} /> : null }
+      <IconButton type="submit" size="large"  aria-label="search" edge="end" onClick={searchMovie}>
+      <SearchIcon sx={{ mt: -1, my: 3}}/>
+      </IconButton>
+  </div>
+  )
 }
--- pages/movie.tsx
+++ pages/movie.tsx
@@ -1,10 +1,13 @@
-import { InputBase } from "@mui/material";
 import { useState } from "react";
 import type { FC } from 'react'
 import { TheHeader } from "../components/TheHeader";
 import { InputMovieValue } from "../components/InputMovieValue";
 
 import CancelIcon from '@mui/icons-material/Cancel';
+import SearchIcon from '@mui/icons-material/Search';
+import CardContent from '@mui/material/CardContent';
+
+import axios from "axios";
 
 interface ValueState {
   searchValue: string;
@@ -14,7 +17,6 @@ interface ValueState {
 const Movie: FC = () => {
 
   const [ searchValue, setSearchValue ] = useState('');
-
   const [ movies, setMovies ] = useState([]);
 
   const handleInputChange = (e: any): void => {
@@ -25,19 +27,20 @@ const Movie: FC = () => {
     setSearchValue('')
   }
 
+  const searchMovie: any = (searchValue): void => {
+    const url = `https://app.rakuten.co.jp/services/api/BooksBook/Search/20130522?applicationId=1086884900424216159&title=${searchValue}&format=json`
+
+    axios.get(url)
+    .then((res) => {
+      console.log(res.data.Items);
+
+    })
+  }
+
   return (
     <div>
-    <TheHeader />
-      <div id="inputBase">
-      <InputBase
-        placeholder="映画タイトル入力"
-        sx={{ ml: 2, flex: 1 }}
-        onChange={handleInputChange}
-        value={searchValue}
-      />
-      { searchValue ?
-        <CancelIcon sx={{ ml: 1, mt: -1, my: -1 }} onClick={resetInput} /> : null }
-      </div>
+      <TheHeader />
+      <InputMovieValue resetInput={resetInput} searchMovie={searchMovie} handleInputChange={handleInputChange} />
     </div>
   )
 }
